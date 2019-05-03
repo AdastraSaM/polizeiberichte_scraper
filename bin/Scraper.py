a389bbd6910477cf121d53acf779030972068b18
AdastraSaM <markus.salomon@adastragrp.com>
2019-05-03 13:46:24 +0200

    Extracted the cleaning and transformation logic from the scraper into its own script.

--- bin/Scraper.py
+++ bin/Scraper.py
@@ -3,7 +3,7 @@ from modules import scraping_tools as st
 
 HOSTNAME = "https://www.presseportal.de"
 START_PAGE = HOSTNAME + "/blaulicht/nr/4970"
-COUNT_OF_SUBSEQUENT_SITES = 20
+COUNT_OF_SUBSEQUENT_SITES = 1
 
 if __name__ == "__main__":
     # Extract data from the web pages
@@ -13,45 +13,21 @@ if __name__ == "__main__":
     all_headlines, all_main_articles, all_websites, all_timestamps = st.get_news_from_links(link_for_all_news)
 
     # Save data in dataframe
-    df = pd.DataFrame({"Timestamp": all_timestamps,
+    berichte = pd.DataFrame({"Timestamp": all_timestamps,
                        "Ueberschrift": all_headlines,
                        "Hauptartikel": all_main_articles,
                        "Link": all_websites})
 
     # Remove control characters
-    st.remove_control_characters(df, "Ueberschrift")
-    st.remove_control_characters(df, "Hauptartikel")
+    st.remove_control_characters(berichte, "Ueberschrift")
+    st.remove_control_characters(berichte, "Hauptartikel")
 
-    # Parse timestamp strings as datetime
-    df["Timestamp"] = st.parse_timestamp(df["Timestamp"])
-
-    # Extract and clean headlines
-    df = st.extract_date_from_column(df, "Ueberschrift")
-    df["Ueberschrift"] = st.clean_headline(df["Ueberschrift"])
-
-    # Extract locations
-    df["Ort"], df["Sekundaerer Ort"] = st.extract_location_from_headline(df["Ueberschrift"])
-    # Remove location from headline
-    df["Ueberschrift"] = df["Ueberschrift"].apply(lambda x: x[x.find(":")+2:])
-
-    # Extract description
-    df["Beschreibung"] = st.extract_description(df["Hauptartikel"])
-
-    # Remove start from the main article
-    df["Hauptartikel"] = st.remove_start_from_main_article(df["Hauptartikel"])
-
-    # Extract place and author from description
-    df["Author"] = st.extract_author(df["Beschreibung"])
-
-    # Remove author, which is the name of the author in brackets
-    df["Beschreibung"] = df["Beschreibung"].apply(lambda x: "".join(x.split(" ")[:-1]))
-
-    df.drop_duplicates(inplace=True)
+    berichte.drop_duplicates(inplace=True)
 
     # Export data
-    df.to_csv(r"../out/Polizeiberichte.csv",
-              sep=";",
-              encoding="UTF-8",
-              header=True,
-              quotechar='"',
-              index=False)
+    berichte.to_csv(r"../out/Polizeiberichte_raw.csv",
+                    sep=";",
+                    encoding="UTF-8",
+                    header=True,
+                    quotechar='"',
+                    index=False)
