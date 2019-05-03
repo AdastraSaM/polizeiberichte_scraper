c63cb47772a512f1d5ae3b5eaaad673f008a0511
AdastraSaM <markus.salomon@adastragrp.com>
2019-05-03 10:23:41 +0200

    All empty test cases will fail now. Added some new tests for find nth occurence.

--- test/scraping_tools_unittests.py
+++ test/scraping_tools_unittests.py
@@ -12,24 +12,30 @@ class ScrapingToolsTest(unittest.TestCase):
         print("Testdata loaded.")
 
     def test_parse_timestamp(self):
-        pass
+        self.fail()
 
     def test_extract_date_from_column(self):
-        pass
+        self.fail()
 
     def test_clean_headline(self):
-        pass
+        self.fail()
 
     def test_find_nth_occurrence(self):
         self.assertEqual(st.find_nth_occurrence("aaaaa", "a", 0), 1)
         self.assertEqual(st.find_nth_occurrence("aaaaa", "a", 1), 2)
         self.assertEqual(st.find_nth_occurrence("bbbbb", "a", 1), -1)
+        self.assertEqual(st.find_nth_occurrence("abc abc abc", "abc", 0), 0)
+        self.assertEqual(st.find_nth_occurrence("abc:-abc:abc ", ":", 0), 3)
+
+
+        self.assertNotEqual(st.find_nth_occurrence("bbbbb", "a", 1), 2)
+
 
     def test_extract_location_from_headline(self):
-        pass
+        self.fail()
 
     def test_extract_description(self):
-        pass
+        self.fail()
 
     def test_extract_place(self):
         self.assertEqual(list(st.extract_place(self.test_berichte["Beschreibung"])),
